5992d3e3124e8b9bf6305df630b3e01c69e8df4d
fazlamehrab <mehrab@vt.edu>
2020-07-29 23:11:06 -0400

    CC flags added

--- gcc8fix.sh
+++ gcc8fix.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-gcc "$@" "-Wno-tautological-compare" "-Wno-attributes" "-Wno-uninitialized"
-#gcc "$@" "-Wno-cast-function-type" "-Wno-tautological-compare" "-Wno-packed-not-aligned" "-Wno-attributes" "-Wno-uninitialized"
+gcc "$@" "-Wno-cast-function-type" "-Wno-tautological-compare" "-Wno-packed-not-aligned" "-Wno-attributes" "-Wno-uninitialized"
